c4158cbd2e1d85329da247b05c4abbb84a2c8347
Qwravanan <jayvi688@gmail.com>
2024-08-02 09:35:56 +0530

    config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7479311534:AAHt1u-rBcXYsTSQk9uXG6qxQ9YqQbp12sE")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("JustVijay786")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5555627697")
 
 
 
